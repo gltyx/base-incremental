63dd9919db66bb4b662da0bb780d3a2dc7d24f3f
Robocittykat <115361685+Robocittykat@users.noreply.github.com>
2024-03-11 20:25:00 -0400

    Update Autosave.js

--- Autosave.js
+++ Autosave.js
@@ -34,5 +34,43 @@ function autosave(){
 	//THE ENTIRE WEBPAGE OH MY GOODNESS!!!
 	localStorage.setItem("webpageState",htmlContent.innerHTML)
 	console.log("saving")
+	//Earlygame variables added in v1.2.0
+	localStorage.setItem("cheaperPointUpgrade",cheaperPointUpgrade)
+	localStorage.setItem("pointsUpgradeEffect",pointsUpgradeEffect)
+	localStorage.setItem("selfSynLog",selfSynLog)
+	localStorage.setItem("selfSynRoot",selfSynRoot)
+	localStorage.setItem("freePointsBuyable",freePointsBuyable)
+	localStorage.setItem("passiveLog",passiveLog)
+	localStorage.setItem("distanceMulti",distanceMulti)
+	localStorage.setItem("pathMulti",pathMulti)
+	//hyperspace variables from v1.2.0
+	localStorage.setItem("hyperEnergy",hyperEnergy)
+	localStorage.setItem("totalHyperspace",totalHyperspace)
+	localStorage.setItem("effectiveHyperspace",effectiveHyperspace)
+	localStorage.setItem("space",space)
+	localStorage.setItem("planes",planes)
+	localStorage.setItem("lines",lines)
+	localStorage.setItem("unspentHyperspace",unspentHyperspace)
+	localStorage.setItem("spaceLoss",spaceLoss)
+	localStorage.setItem("planeLoss",planeLoss)
+	localStorage.setItem("lineLoss",lineLoss)
+	localStorage.setItem("maxBase",maxBase)
+	localStorage.setItem("unboughtHyperUpgrades",unboughtHyperUpgrades)
+	localStorage.setItem("hyperLogMulti",hyperLogMulti)
+	localStorage.setItem("strongPassiveLog",strongPassiveLog)
+	localStorage.setItem("incompleteBranchChallenges",incompleteBranchChallenges)
+	localStorage.setItem("bestLineProd",bestLineProd)
+	localStorage.setItem("bestLines",bestLines)
+	localStorage.setItem("hyperIpsis",hyperIpsis)
+	localStorage.setItem("hyperMaxBase",hyperMaxBase)
+	localStorage.setItem("hyperMulti",hyperMulti)
+	//checkpoint
+	if(totalPaths >= 8 || hyperEnergy >= 1 || totalHyperspace >= 1){
+		localStorage.setItem("checkpoint","hyper")
+	}else if(logUnlocked){
+		localStorage.setItem("checkpoint","log")
+	}else{
+		localStorage.setItem("checkpoint","base")
+	}
 }
 setInterval(autosave,30000)
